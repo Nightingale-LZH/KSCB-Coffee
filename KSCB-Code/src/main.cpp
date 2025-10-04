181d4d5e3c62f8a43470a824be219104d8fe4009
Zihui Liu <44153062+Nightingale-LZH@users.noreply.github.com>
2025-10-03 18:28:36 -0700

    Started FSM

--- KSCB-Code/src/main.cpp
+++ KSCB-Code/src/main.cpp
@@ -1,12 +1,4 @@
-#include <Arduino.h>
-
-#define PIN_L298N_ENA 3
-#define PIN_L298N_IN1 2
-#define PIN_L298N_IN2 4
-
-#define PIN_BUILTIN_LED 13
-
-#define PIN_POT_SPEED A0
+#include "main.h"
 
 void setup() {
     pinMode(PIN_L298N_ENA, OUTPUT);
---
+++ KSCB-Code/src/main.h
@@ -0,0 +1,16 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <Arduino.h>
+
+#define PIN_L298N_ENA 3
+#define PIN_L298N_IN1 2
+#define PIN_L298N_IN2 4
+
+#define PIN_BUILTIN_LED 13
+
+#define PIN_POT_SPEED A0
+
+
+
+#endif  // __MAIN_H__
