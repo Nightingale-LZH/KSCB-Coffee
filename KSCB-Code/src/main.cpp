f03c0b9fef27e37c9e733528534022e6c2a7e638
Zihui Liu <44153062+Nightingale-LZH@users.noreply.github.com>
2025-10-08 11:14:49 -0700

    Finished Test Code

--- KSCB-Code/src/main.cpp
+++ KSCB-Code/src/main.cpp
@@ -1,29 +1,136 @@
 #include "main.h"
 
+#ifdef DEBUG_IO
+
+//  DEBUG OPTIONS
+// #define DEBUG_LED
+// #define DEBUG_MOTOR
+// #define DEBUG_BUTTON
+#define DEBUG_DISPLAY
+
 void setup() {
+    #ifdef DEBUG_MOTOR
     pinMode(PIN_L298N_ENA, OUTPUT);
     pinMode(PIN_L298N_IN1, OUTPUT);
     pinMode(PIN_L298N_IN2, OUTPUT);
-    pinMode(PIN_BUILTIN_LED, OUTPUT);
 
     // Motor Forward Direcction
     digitalWrite(PIN_L298N_IN1, HIGH);
     digitalWrite(PIN_L298N_IN2, LOW);
+    #endif  //  DEBUG_MOTOR
+
+
+    #ifdef DEBUG_LED
+    pinMode(PIN_LED_BUILTIN, OUTPUT);
+    pinMode(PIN_LED_BREW  , OUTPUT);
+    pinMode(PIN_LED_FIN   , OUTPUT);
+    pinMode(PIN_LED_MOTOR , OUTPUT);
+    pinMode(PIN_LED_ML    , OUTPUT);
+    pinMode(PIN_LED_HR_MIN, OUTPUT);
+    #endif  //  DEBUG_LED
+
+
+    #ifdef DEBUG_BUTTON
+    Serial.begin(9600);
+    pinMode(PIN_BTN_CHANGE, INPUT_PULLUP);
+    pinMode(PIN_BTN_START , INPUT_PULLUP);
+    pinMode(PIN_BTN_PLUS  , INPUT_PULLUP);
+    pinMode(PIN_BTN_MINUS , INPUT_PULLUP);
+    #endif  //  DEBUG_BUTTON
+
+
+    #ifdef DEBUG_DISPLAY
+    
+    #endif  //  DEBUG_DISPLAY
+}
+
+void loop() {
+    #ifdef DEBUG_MOTOR
+    digitalWrite(PIN_L298N_ENA, LOW);
+    delay(2500);
+
+    digitalWrite(PIN_L298N_ENA, HIGH);
+    delay(500);
+    #endif  //  DEBUG_MOTOR
+
+
+    #ifdef DEBUG_LED
+    digitalWrite(PIN_LED_BUILTIN, HIGH);
+    digitalWrite(PIN_LED_BREW  , HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_FIN   , HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_MOTOR , HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_HR_MIN, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED_ML    , HIGH);
+    delay(1000);
+
+    digitalWrite(PIN_LED_BUILTIN, LOW);
+    digitalWrite(PIN_LED_BREW  , LOW);
+    digitalWrite(PIN_LED_FIN   , LOW);
+    digitalWrite(PIN_LED_MOTOR , LOW);
+    digitalWrite(PIN_LED_ML    , LOW);
+    digitalWrite(PIN_LED_HR_MIN, LOW);
+    delay(1000);
+    #endif  //  DEBUG_LED
+
+
+    #ifdef DEBUG_BUTTON
+    if (!digitalRead(PIN_BTN_CHANGE)) { Serial.println("CHANGE"); }
+    if (!digitalRead(PIN_BTN_START )) { Serial.println("START"); }
+    if (!digitalRead(PIN_BTN_PLUS  )) { Serial.println("PLUS"); }
+    if (!digitalRead(PIN_BTN_MINUS )) { Serial.println("MINUS"); }
+    Serial.println("---");
+    delay(1000);
+    #endif  //  DEBUG_BUTTON
+
+
+    #ifdef DEBUG_DISPLAY
+    display.setBrightness(0x0f);
+    
+    display.showNumberDecEx(1, 0b10000000);
+    delay(1000);
+    display.showNumberDecEx(2, 0b01000000);
+    delay(1000);
+    display.showNumberDecEx(3, 0b00100000);
+    delay(1000);
+    display.showNumberDecEx(4, 0b00010000);
+    delay(1000);
+    #endif  //  DEBUG_DISPLAY
+}
+
+#else
+
+void setup() {
+    // pinMode(PIN_L298N_ENA, OUTPUT);
+    // pinMode(PIN_L298N_IN1, OUTPUT);
+    // pinMode(PIN_L298N_IN2, OUTPUT);
+    // pinMode(PIN_BUILTIN_LED, OUTPUT);
+
+    // // Motor Forward Direcction
+    // digitalWrite(PIN_L298N_IN1, HIGH);
+    // digitalWrite(PIN_L298N_IN2, LOW);
 
 }
 
 void loop() {
-    unsigned int delay_interval = map(
-        analogRead(PIN_POT_SPEED),
-        0, 1024, 
-        0, 10000
-    );
+    // unsigned int delay_interval = map(
+    //     analogRead(PIN_POT_SPEED),
+    //     0, 1024, 
+    //     0, 10000
+    // );
 
-    digitalWrite(PIN_L298N_ENA, LOW);
-    digitalWrite(PIN_BUILTIN_LED, LOW);
-    delay(delay_interval);
+    // digitalWrite(PIN_L298N_ENA, LOW);
+    // digitalWrite(PIN_BUILTIN_LED, LOW);
+    // delay(delay_interval);
 
-    digitalWrite(PIN_L298N_ENA, HIGH);
-    digitalWrite(PIN_BUILTIN_LED, HIGH);
-    delay(200);
+    // digitalWrite(PIN_L298N_ENA, HIGH);
+    // digitalWrite(PIN_BUILTIN_LED, HIGH);
+    // delay(200);
+
 }
+
+#endif // DEBUG_IO
+
--- KSCB-Code/src/main.h
+++ KSCB-Code/src/main.h
@@ -2,15 +2,35 @@
 #define __MAIN_H__
 
 #include <Arduino.h>
+#include "TM1637Display.h"
 
-#define PIN_L298N_ENA 3
-#define PIN_L298N_IN1 2
-#define PIN_L298N_IN2 4
+#define DEBUG_IO
 
-#define PIN_BUILTIN_LED 13
+#define PIN_LED_BUILTIN 13
 
-#define PIN_POT_SPEED A0
+#define PIN_LED_BREW    6
+#define PIN_LED_FIN     5
+#define PIN_LED_MOTOR   4
+#define PIN_LED_HR_MIN  3
+#define PIN_LED_ML      2
 
+#define PIN_BTN_CHANGE  14
+#define PIN_BTN_START   17
+#define PIN_BTN_PLUS    15
+#define PIN_BTN_MINUS   16
+
+#define PIN_L298N_ENA   11
+#define PIN_L298N_IN1   10
+#define PIN_L298N_IN2   9
+
+#define PIN_TM1637_DIO  12
+#define PIN_TM1637_CLK  7
+
+
+
+#define TM1637_DOT_BYTECODE 0b01000000
+
+TM1637Display display(PIN_TM1637_CLK, PIN_TM1637_DIO);
 
 
 #endif  // __MAIN_H__
